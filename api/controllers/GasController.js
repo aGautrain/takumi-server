3ecfec9d177dbd3fbf246491f2134cadaa4904af
Darkoeur <antoine.gautrain@gmail.com>
2022-06-27 01:27:01 +0200

    chore: init sails

---
+++ api/controllers/GasController.js
@@ -0,0 +1,77 @@
+/**
+ * GasController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+const axios = require('axios');
+
+const symbolConverterForOwlracle = {
+    FTM: 'ftm',
+    MATIC: 'poly',
+    AVAX: 'avax',
+    ETH: 'eth',
+    BNB: 'bsc',
+};
+
+// if we fetched gas from owlracle more than 15 minutes ago, we will refresh stored data
+const GAS_CACHE_MAX_DELAY = 15 * 60;
+
+module.exports = {
+
+    getGas: async function (req, res) {
+
+        const symbol = req.param('symbol');
+        const owlracleSymbol = symbolConverterForOwlracle[symbol];
+
+        if (!owlracleSymbol) sails.log.error('unsupported symbol, cannot return gas');
+
+        const apiEndpoint = 'https://owlracle.info/' + owlracleSymbol + '/gas';
+
+        const cachedGas = await Gas.findOne({ symbol });
+
+        if (cachedGas) {
+            sails.log.info(cachedGas);
+
+            const latestDateWeFetchedGas = new Date(cachedGas.timestamp);
+
+            const delay = Date.now() / 1000 - latestDateWeFetchedGas.getTime() / 1000;
+
+            sails.log.info('Fetched gas for ' + symbol + ' ' + Math.round(delay) + ' seconds ago');
+
+            if (delay < GAS_CACHE_MAX_DELAY) {
+                sails.log.info('Returning cached gas', cachedGas);
+                return res.json(cachedGas);
+            }
+        }
+
+        sails.log.info('Trying to fetch gas from Owlracle API');
+        return axios
+            .get(apiEndpoint, { params: { apikey: '' } })
+            .then(async owlracleResponse => {
+                if (owlracleResponse?.data) sails.log.info(owlracleResponse.data);
+                else return res.badRequest();
+
+                const gasToStore = {
+                    ...owlracleResponse.data,
+                    symbol
+                };
+
+                await Gas.create(gasToStore);
+
+                sails.log.info('Returning latest gas', gasToStore);
+
+
+                return res.json(gasToStore);
+            })
+            .catch(error => {
+                sails.log.error(error);
+                sails.log.error('Error fetching gas for symbol', symbol);
+                return res.badRequest();
+            });
+    }
+
+
+};
+
---
+++ api/models/Gas.js
@@ -0,0 +1,63 @@
+/**
+ * Gas.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    symbol: {
+      type: 'string',
+      unique: true,
+      required: true
+    },
+
+    avgGas: {
+      type: 'number'
+    },
+
+    avgTime: {
+      type: 'number'
+    },
+
+    avgTx: {
+      type: 'number'
+    },
+
+    baseFee: {
+      type: 'number'
+    },
+
+    lastBlock: {
+      type: 'number'
+    },
+
+    speeds: {
+      type: 'json'
+    },
+
+    timestamp: {
+      type: 'string'
+    }
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
